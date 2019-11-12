ff4c006182e65ea396d203b5ba6f70e5b7ab60a6
guopan@taihemedia.com <guopan@taihemedia.com>
2019-11-12 17:12:29 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,10 @@
+//index.js
+//获取应用实例
+
+Page({
+  data: {
+    text: '此处并没有强制要求 node_modules 必须在小程序根目录下（即 project.config.js 中的 miniprogramRoot 字段），也可以存在于小程序根目录下的各个子目录中。但是不允许 node_modules 在小程序根目录外。',
+  },
+  onLoad: function () {
+  },
+})
